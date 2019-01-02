a8a9f384727fd4e3a08a9ebd7fd42f2353488e97
Bruno Morais <bruno.morais@inf.ufrgs.br>
2019-01-02 17:28:04 -0200

    Criação de objetos simples JAVA

--- java/helloWorld.java
+++ java/helloWorld.java
@@ -1,6 +1,12 @@
+import java.io.PrintStream;
+
 public class helloWorld
 {
   public static void main(String[] args) {
+
+    PrintStream printer = System.out;
     System.out.println("Hello World !!");
+
+    printer.println("heloooooo");
   }
 }
---
+++ java/retangulo.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class retangulo
+{
+  public static void main(String[] args) {
+    Rectangle box = new Rectangle(5,10,20,30);
+    Rectangle box2 = box;
+  System.out.println(box);
+
+  System.out.println(box2);
+
+
+  }
+}
