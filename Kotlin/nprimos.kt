d2c9cfdffd0db2f9161e131a2bbd0bd037bbdbe5
Bruno Morais <bruno.morais@inf.ufrgs.br>
2019-07-05 17:53:57 -0300

    Algumas modificações

--- Kotlin/nprimos.kt
+++ Kotlin/nprimos.kt
@@ -4,9 +4,18 @@ fun main(){
   println("Digite um numero: ")
   val n = readLine()!!.toInt()
 
+  var primo = true
 
-  println(n)
+  for (i in 2..n-1)
+    if ((n % i) == 0 ) {
+      primo = false
 
+    }
+
+  if (primo)
+    print("É primo")
+  else
+    print("Nao eh primo")
 
 
   }
