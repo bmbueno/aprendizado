4750990d231040052b55c76d45c0293e0e2df0a7
Bruno Morais <bruno.morais@inf.ufrgs.br>
2019-07-15 17:46:37 -0300

    Inicializaçao do projeto, arquivo de testes de OO

---
+++ Kotlin/Project/teste.kt
@@ -0,0 +1,13 @@
+class book {
+  var titulo: String
+  var numero: Int
+
+  constructor(titulo: String, numero: Int){
+    this.titulo = titulo
+    this.numero = numero
+  }
+}
+
+fun main(){
+  print("teste")
+}
